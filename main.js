4e768fbe9e7e3a0f4f7e911bc3bb1ab197614441
Ramsey Jordan <ramsey.jordan.84@gmail.com>
2018-11-20 22:25:16 -0500

    added new files to the old repo

--- main.js
+++ main.js
@@ -1,22 +1,83 @@
-document.getElementById('timer').innerHTML =
-  20 + ":" + 00;
-startTimer();
-
-function startTimer() {
-  var presentTime = document.getElementById('timer').innerHTML;
-  var timeArray = presentTime.split(/[:]+/);
-  var m = timeArray[0];
-  var s = checkSecond((timeArray[1] - 1));
-  if(s==59){m=m-1}
-  //if(m<0){alert('timer completed')}
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', function(e){
+  // Hide Results
+  document.getElementById('results').style.display = 'none';
   
-  document.getElementById('timer').innerHTML =
-    m + ":" + s;
-  setTimeout(startTimer, 1000);
+  // Show Loader
+  document.getElementById('loading').style.display = 'block';
+
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
+
+// calculate Results
+function calculateResults(){
+  console.log('Calculating..');
+  // UI Vars
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 /12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+  
+
+  // Compute Monthly Payments
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if(isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+    // Show Results
+    document.getElementById('results').style.display = 'block';
+
+    // Hide Loading
+    document.getElementById('loading').style.display = 'none';
+
+
+  } else {
+    showError('Please Check your numbers')
+
+        // Hide Results
+        document.getElementById('results').style.display = 'none';
+
+        // Hide Loading
+        document.getElementById('loading').style.display = 'none';
+  }
 }
 
-function checkSecond(sec) {
-  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
-  if (sec < 0) {sec = "59"};
-  return sec;
+
+// Show Error
+function showError(error) {
+  const errorDiv = document.createElement('div');
+
+  // Get Element
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  // Add Class
+  errorDiv.className = 'alert alert-danger';
+
+  // Create Text Node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert Pre Heading Error
+  card.insertBefore(errorDiv, heading);
+
+  // Clear Err message after 3s
+  setTimeout(clearError, 2000);
+
+  // Clr Err
+  function clearError(){
+    document.querySelector('.alert').remove();
+  }
 }
